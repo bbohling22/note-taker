a7f8daf1bcd8075bab8ad3a227195f4aacc5f55e
bbohling22 <bbohling22@gmaol.com>
2023-03-15 16:09:17 -0700

    finished store.js file

---
+++ routes/homeroutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const path = require("path");
+
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+module.exports(PORT, () => console.log(`Listening on port: ${PORT}`));
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const homeRoutes = require('./routes/homeroutes')
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static("public"));
+
+app.use('/', homeRoutes);
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
