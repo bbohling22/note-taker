4a3f7ff1cd6b851d9c8eb8b27800a2185ff5a464
bbohling22 <bbohling22@gmaol.com>
2023-04-12 15:10:41 -0700

    Just pushing what i have'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const homeRoutes = require('./routes/homeroutes')
 
 const app = express();
 
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
